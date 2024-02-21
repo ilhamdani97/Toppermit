bd2cc6377b2ad5f76236fbef10f28000610ce706
Ilham Ramadani <ramadaniilham63@gmail.com>
2024-02-21 10:28:40 +0700

    question 1

---
+++ FindFirsWordToUpperCase(1).js
@@ -0,0 +1,12 @@
+handleGetFirstAfterDash('Ilham-ramadani')
+
+function handleGetFirstAfterDash(value){
+    const splitValue = value.split('-');
+    let valueFinal = '';
+    
+    splitValue.forEach((data) => {
+        valueFinal += data.charAt(0).toUpperCase()
+    })
+    
+    console.log("final value", valueFinal)
+}
